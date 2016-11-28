b36741ddf93cb343102633b41e415d63e4b26de9
brandonxiang <1542453460@qq.com>
2016-11-28 15:58:01 +0800

    add a boolean jsx

---
+++ src/fields/boolean.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Switch } from 'antd'
+import Pop from './pop'
+
+function onChange(checked) {
+    console.log('switch to ${checked}');
+}
+
+class Switch extends React.Component{
+    static propTypes = {
+        onChange: React.PropTypes.func.isRequired,
+        name: React.PropTypes.string.isRequired,
+        value: React.PropTypes.string,
+        default: React.PropTypes.number,
+        doc: React.PropTypes.string,
+   }
+
+    render() {
+          const content = (
+			<div>
+				<p>Content</p>
+				<p>Content</p>
+			</div>
+			);
+
+         const input = <Switch defaultChecked={false} onChange={onChange} />
+     
+         return <Pop
+             name={this.props.name}
+             content={content}
+             input = {input}
+             />
+    }
+}
